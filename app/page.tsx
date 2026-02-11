5f23b1e0dca3577b9dc566ec3600b76e2f90123f
Angusb111 <bodleangus@gmail.com>
2026-02-12 01:29:39 +1300

    v1.07 bugfix

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import { color } from "framer-motion";
 import Image from "next/image";
 import { Footer } from "@/components/Footer";
 
-
 export default function Home() {
 return (
 <main className="">
